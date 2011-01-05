3d4161c317f0973be4fcb12f58565e0d18a06b8a
Martin Langhoff <martin@laptop.org>
2011-01-05 16:41:44 -0500

    Peru 10.1.2 -- cleanup, translations
     - Added the translations in a portable manner
     - cleaned up invalid parts of the ini file
     - added xorg-dcon.conf patch
    
    It is almost trivial to apply this on 10.1.3

--- examples/peru/pri-0.84.sh
+++ examples/peru/pri-0.84.sh
@@ -1,19 +1,30 @@
-#!/bin/sh
+#!/bin/bash -x
+
+set -e
 
 ##################CAMBIOS EN SUGAR##########################
 
-#Cambiar las traducciones
-#cp -f /home/aliosh/src/custom/0.84/lang/sugar-toolkit.mo "$INSTALL_ROOT"/usr/share/locale/es/LC_MESSAGES/sugar-toolkit.mo
-cp -f /home/aliosh/src/xo-peru/custom/0.84/lang/sugar.mo "$INSTALL_ROOT"/usr/share/locale/es/LC_MESSAGES/sugar.mo
+CONFDIR=/home/martin/src/olpc-os-builder/examples/peru
+for F in sugar sugar-toolkit; do
+    # extract
+    msgunfmt "$INSTALL_ROOT"/usr/share/locale/es/LC_MESSAGES/$F.mo > /tmp/$F-orig.po
+    # merge with new definitions
+    msgmerge $CONFDIR/$F.po /tmp/$F-orig.po > /tmp/$F-new.po
+    # compile & install 
+    msgfmt /tmp/$F-new.po > "$INSTALL_ROOT"/usr/share/locale/es/LC_MESSAGES/$F.mo
+done
 
 #patch para vnc
-patch -p0 "$INSTALL_ROOT"/etc/X11/xorg-dcon.conf /home/aliosh/src/xo-peru/custom/x11/enablextest.patch
+#patch -p0 "$INSTALL_ROOT"/etc/X11/xorg-dcon.conf /home/aliosh/src/xo-peru/custom/x11/enablextest.patch
+
+# chop the empty line in /etc/issue
 sed -i -e '3d'  "$INSTALL_ROOT"/etc/issue
+# Add credits
 echo "Personalizado para PERU por:"  >> "$INSTALL_ROOT"/etc/issue
 echo "Aliosh Neira(aliosh2006@gmail.com) y Hernan Pachas(hernan.pachas@gmail.com)" >> "$INSTALL_ROOT"/etc/issue
 echo -e "\n" >> "$INSTALL_ROOT"/etc/issue
 
-#ajuste puntero touchpad
 
+#ajuste puntero touchpad
 echo 'xset m 5 1' >> "$INSTALL_ROOT"/home/olpc/.xsession
 
