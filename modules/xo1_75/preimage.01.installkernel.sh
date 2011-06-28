67256491f377ea2da9fd515b8f7a17a3d5fe7a37
Martin Langhoff <martin@laptop.org>
2011-06-28 15:55:06 -0400

    Introducing: xo1_75 module
    Use when spinning an img for XO-1.75. Currently carries some hacks
    that I expect to clear up soon.

---
+++ modules/xo1_75/preimage.01.installkernel.sh
@@ -0,0 +1,28 @@
+# Copyright (C) 2009 One Laptop Per Child
+# Licensed under the terms of the GNU GPL v2 or later; see COPYING for details.
+
+# this must be run before the base module creates versioned fs layout
+
+. $OOB__shlib
+
+#echo "Installing kernel rpm manually..."
+rpm --root "$fsmount" --ignorearch -ivh $oob_config_dir/kernel-2.6.39_xo1.75-20110621.1628.olpc.2b77a83.armv7l.rpm
+
+# FIXME olpc.fth usually comes in the bootfw rpm
+# This olpc.fth compensates for lack of /ofw to trigger runin
+cat > $fsmount/boot/olpc.fth <<EOF
+\ olpc.fth
+: runin? " TS" find-tag if
+        ?-null " RUNIN" nocase-$= if
+                true
+                exit
+        then
+  then
+  false
+;
+
+" root=/dev/mmcblk0p2 console=ttyS2,115200 console=tty0 loglevel=9 rootwait debug " to boot-file
+runin? if boot-file "  runin" $cat2 to boot-file then
+boot last:\vmlinuz
+
+EOF
