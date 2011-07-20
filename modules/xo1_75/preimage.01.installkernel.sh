00c0c02f2bf09ca7522cd8f3a77a9403edcab73c
Martin Langhoff <martin@laptop.org>
2011-07-19 21:57:43 -0400

    xo 1.75: fix kernel rpm handling
    The build host must be able to install and armv7l kernel. Frob
    /etc/rpm/platform on the host if needed.
    
    With this we can remove some ugly workarounds.

--- modules/xo1_75/preimage.01.installkernel.sh
+++ modules/xo1_75/preimage.01.installkernel.sh
@@ -5,9 +5,6 @@
 
 . $OOB__shlib
 
-#echo "Installing kernel rpm manually..."
-rpm --root "$fsmount" --ignorearch -ivh $oob_config_dir/kernel-2.6.39_xo1.75-20110621.1628.olpc.2b77a83.armv7l.rpm
-
 # FIXME olpc.fth usually comes in the bootfw rpm
 # This olpc.fth compensates for lack of /ofw to trigger runin
 cat > $fsmount/boot/olpc.fth <<"EOF"
